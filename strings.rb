ac464b89461ee7bbda7ea08082475652a3bd43db
Andre Christoga <andrechristoga@gmail.com>
2015-12-10 10:06:27 +0700

    Strings script

---
+++ strings.rb
@@ -0,0 +1,21 @@
+people = 10
+x = "There are #{people} types of people"
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}"
+
+puts x
+puts y
+
+puts "I said: #{x}"
+puts "I also said: #{y}"
+
+hillarious = false
+joke = "Isn't that joke is funny? #{hillarious}"
+
+puts joke
+
+w = "Left side"
+e = "Right side"
+
+puts w + e
